bea2a767b8b22be877bcac70a0788b93b2828964
jpais01 <jpais01@ucm.es>
2026-05-12 21:19:43 +0200

    constructor

--- includes/Formulario/FormularioElegirMesa.php
+++ includes/Formulario/FormularioElegirMesa.php
@@ -6,6 +6,12 @@ require_once __DIR__ . '/../../includes/PedidoService.php';
 
 class FormularioElegirMesa extends Formulario
 {
+    
+
+    public function __construct() {
+        parent::__construct('formElegirMesa'); 
+    }
+
     protected function generaCamposFormulario(&$datos)
     {
         $mesas = \PedidoService::getMesasDisponibles();
